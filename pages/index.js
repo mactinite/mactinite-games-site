7fea05010935aad68b7a3318277ae7f94813b9d8
Hunter MacDonald <mactinite77@gmail.com>
2021-08-21 16:25:00 -0700

    delete all the starter stuff and put in placeholder text

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,9 @@
-import SmallCard from '../components/SmallCard';
-import { projectIcons } from '../components/Icons';
-
-import { projects } from '../utils/projectsData';
 
 const Home = () => (
   <div className="home">
-    <h1>What Can I Deploy to Static Apps?</h1>
-    <div className="card-grid">
-      {projects.map((project) => {
-        const Icon = projectIcons[project.id];
-        return <SmallCard key={project.id} Icon={Icon} title={project.name} slug={project.slug} />;
-      })}
-    </div>
+      Mactinite Games
+      website
+      yes this is it 
   </div>
 );
 
