a3e253bc16bb62840b96abbe5b4be681da7aa942
Hunter MacDonald <mactinite77@gmail.com>
2021-08-21 20:30:02 -0700

    Gunship feature and nice header

---
+++ components/HeroHeader.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useEffect, useLayoutEffect, useState, useRef } from "react";
+let scrollPosition = 0;
+export const HeroHeader = (props = { scrollAction: 250, children: null }) => {
+    const _containerDOM = useRef(null);
+    const [height, setHeight] = useState(undefined)
+    
+
+    useEffect(() => {
+        
+        window.addEventListener('scroll', onScroll)
+        return function cleanup() {
+            window.removeEventListener('scroll', onScroll)
+        };
+    })
+
+    const onScroll = () => {
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        if (props.scrollAction >= scrollTop) {
+            const step = scrollPosition - scrollTop;
+            const actualHeight = _containerDOM.current.offsetHeight;
+            const height = actualHeight + step;
+            setHeight(height);
+            scrollPosition = scrollTop;
+        }
+    }
+    return (
+        <>
+            <div className="hero" style={{ height: height + "px" }} ref={_containerDOM}>
+                <div className='hero-content'>
+                    {props.children}
+                </div>
+            </div>
+            <div style={{ height: props.scrollAction + "px" }}>
+            </div>
+        </>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,48 @@
+import { useEffect, useState, useRef } from "react";
+import { HeroHeader } from '../components/HeroHeader';
+const Home = () => {
 
-const Home = () => (
-  <>
-  <div className="hero">
-    <img src ="/mactinite_games.png"/>
-  </div>
-  </>
-);
+
+  return (
+    <>
+      <HeroHeader scrollAction={150}>
+        <img src="/mactinite_games.png" alt="Mactinite Games" style={{ height: "100%" }} />
+      </HeroHeader>
+      <div className="feature space-bg">
+        <div className="container center" >
+        <h1>COMING SOON</h1>
+          <img src="/GunshipLogo_Transparent.png" alt="Gunship: Zero G combat" style={{ height: "100%", maxWidth: "100%", marginBottom: "100px" }} />
+        </div >
+
+      </div>
+
+
+      <div className="feature">
+        <div className="background-dark">
+          <div className="container center">
+            <br />
+            <p>Gunship is an intense action adventure game with a unique blend of physics based movement and top-down shooter mechanics.</p>
+            <br/>
+            <br/>
+            <a href="https://mactinite.itch.io/gunship" className="button">Try out the demo today!</a>
+            <h1>Fly</h1>
+            <p>
+              Avoid lasers, dodge missiles but be careful not to crash!
+            </p>
+            <h1>Fight</h1>
+            <p>
+              With a large assortment of Weapons and a wide array of "CPU Chips" you can tweak the combat to your style.
+            </p>
+            <h1>Explore</h1>
+            <p>
+              Set in a massive abandoned alien space station, explore and find secrets that will aid you in your mission, and uncover the truth of The Station.
+            </p>
+            <br />
+          </div >
+        </div>
+      </div>
+    </>
+  )
+};
 
 export default Home;
