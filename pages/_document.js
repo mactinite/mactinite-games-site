b7fb90d312313f6749bbceae041f7d8767582cbd
Hunter MacDonald <mactinite77@gmail.com>
2021-09-02 21:39:04 -0700

    Add google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -22,6 +22,15 @@ class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
             rel="stylesheet" />
+          {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-GKBHBKX7HB"></script>
+          <script>{`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-GKBHBKX7HB');
+          `}</script>
         </Head>
         <body>
           <Main />
